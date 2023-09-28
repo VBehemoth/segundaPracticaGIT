62e7959d476f64ec912ebcec9a4c491546adac98
Ana Fernandez <anab.fernandezg@gmail.com>
2023-09-27 19:20:05 -0500

    Commit de métodos suma y resta

--- JavaApplication92/src/Calculator.java
+++ JavaApplication92/src/Calculator.java
@@ -10,4 +10,13 @@
  */
 public class Calculator {
     
+    public int suma (int num1, int num2) {
+    int suma = num1 + num2;
+    return suma;
+}
+    public int resta (int num1, int num2) {
+        
+        int resta = num1 - num2;
+        return resta;
+    }
 }
